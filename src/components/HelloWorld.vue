5bca9569924087c4fee43016dcc93192e4c5f07f
e10chen <75030964+e10chen@users.noreply.github.com>
2023-02-07 22:02:37 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,18 +1,31 @@
 <template>
-  <h1>{{ msg }}</h1>
+    
+<button id="copy" class="buttons">Copy</button>
 
-  <p>
-    <a href="https://vitejs.dev/guide/features.html" target="_blank">
-      Vite Documentation
-    </a>
-    |
-    <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Documentation</a>
-  </p>
+<button id="toggleBackGroundColor" class = "buttons">Change Background Color</button>
 
-  <button type="button" @click="state.count++">count is: {{ state.count }}</button>
+<button id="changeTitle" class = "buttons">Change Title</button>
+
+<button id="deleteButton" class = "buttons">Delete Last Instance</button>
+
+<div class="all">
+<header class ="name">
+  <h1 id = "mainTitle" class="title">Ethan Chen</h1>
+  </header>
+<div class="content">
+  <img src = "https://media.ed.edmunds-media.com/toyota/tacoma/2020/oem/2020_toyota_tacoma_crew-cab-pickup_trd-off-road_fq_oem_1_1600.jpg" width=200px>
+    
   <p>
-  HELLO WORLD
+The image above is a Tacoma pickup truck made by the Japanese automaker Toyota. Toyota has been making the Tacoma pickup truck since 1995. The Tacoma has two engines options: 2.7-liter 4-Cylinder and the 3.5-liter V6. It has a miles per gallon of 20 in the city and 23 on the highway.
   </p>
+  
+    <button id = "detailBtn" class="detailButton">Detail</button>
+   </div>
+   </div>
+  
+
+
+
 </template>
 
 <script setup>
