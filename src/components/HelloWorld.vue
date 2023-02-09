9b36fc2e063a87ec3d3b7ca4519c116c9f6ca9f6
e10chen <75030964+e10chen@users.noreply.github.com>
2023-02-09 10:45:46 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,46 +1,44 @@
 <template>
     
-<div class="buttons">
-  <button class="copy" >Copy</button>
-
-  <button class="toggleBackGroundColor" >Change Background Color</button>
-
-  <button class="changeTitle">Change Title</button>
-
-  <button class="deleteButton">Delete Last Instance</button>
-
-</div>
-
-<div class="all">
-  <h1 class="title">
-    Ethan Chen
-  </h1>
+  <div class="buttons">
+    <button class="copy" >Copy</button>
   
-  <img class ="picture" src = "https://media.ed.edmunds-media.com/toyota/tacoma/2020/oem/2020_toyota_tacoma_crew-cab-pickup_trd-off-road_fq_oem_1_1600.jpg" width=200px>
+    <button class="toggleBackGroundColor" >Change Background Color</button>
+  
+    <button class="changeTitle">Change Title</button>
+  
+    <button class="deleteButton">Delete Last Instance</button>
+  
+  </div>
+  
+  <div class="all">
+    <h1 class="title">
+      Ethan Chen
+    </h1>
     
-  <p class ="text">
-The image above is a Tacoma pickup truck made by the Japanese automaker Toyota. Toyota has been making the Tacoma pickup truck since 1995. The Tacoma has two engines options: 2.7-liter 4-Cylinder and the 3.5-liter V6. It has a miles per gallon of 20 in the city and 23 on the highway.
-  </p>
-    <button class="detailButton">      
-      Detail
-  </button>
-
-</div>
-
-</template>
-
-<script setup>
-import { defineProps, reactive } from 'vue'
-
-defineProps({
-  msg: String
-})
-
-const state = reactive({ count: 0 })
-</script>
-
-<style scoped>
-a {
-  color: #42b983;
-}
-</style>
+    <img class ="picture" src = "https://media.ed.edmunds-media.com/toyota/tacoma/2020/oem/2020_toyota_tacoma_crew-cab-pickup_trd-off-road_fq_oem_1_1600.jpg" width=200px>
+      
+    <p class ="text">
+  The image above is a Tacoma pickup truck made by the Japanese automaker Toyota. Toyota has been making the Tacoma pickup truck since 1995. The Tacoma has two engines options: 2.7-liter 4-Cylinder and the 3.5-liter V6. It has a miles per gallon of 20 in the city and 23 on the highway.
+    </p>
+      <button class="detailButton">      
+        Detail
+    </button>
+  
+  </div>
+  
+  </template>
+  
+  <script setup>
+  import { defineProps, reactive } from 'vue'
+  defineProps({
+    msg: String
+  })
+  const state = reactive({ count: 0 })
+  </script>
+  
+  <style scoped>
+  a {
+    color: #42b983;
+  }
+  </style>
