a107cd652d36f7fbd54236bc148e19d7bb1764f1
e10chen <75030964+e10chen@users.noreply.github.com>
2023-02-09 10:29:06 -0500

    Update HelloWorld.vue

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,30 +1,31 @@
 <template>
     
-<button id="copy" class="buttons">Copy</button>
+<div class="buttons">
+  <button class="copy" >Copy</button>
 
-<button id="toggleBackGroundColor" class = "buttons">Change Background Color</button>
+  <button class="toggleBackGroundColor" >Change Background Color</button>
 
-<button id="changeTitle" class = "buttons">Change Title</button>
+  <button class="changeTitle">Change Title</button>
 
-<button id="deleteButton" class = "buttons">Delete Last Instance</button>
+  <button class="deleteButton">Delete Last Instance</button>
+
+</div>
 
 <div class="all">
-<header class ="name">
-  <h1 id = "mainTitle" class="title">Ethan Chen</h1>
-  </header>
-<div class="content">
-  <img src = "https://media.ed.edmunds-media.com/toyota/tacoma/2020/oem/2020_toyota_tacoma_crew-cab-pickup_trd-off-road_fq_oem_1_1600.jpg" width=200px>
+  <h1 class="title">
+    Ethan Chen
+  </h1>
+  
+  <img class ="picture" src = "https://media.ed.edmunds-media.com/toyota/tacoma/2020/oem/2020_toyota_tacoma_crew-cab-pickup_trd-off-road_fq_oem_1_1600.jpg" width=200px>
     
-  <p>
+  <p class ="text">
 The image above is a Tacoma pickup truck made by the Japanese automaker Toyota. Toyota has been making the Tacoma pickup truck since 1995. The Tacoma has two engines options: 2.7-liter 4-Cylinder and the 3.5-liter V6. It has a miles per gallon of 20 in the city and 23 on the highway.
   </p>
-  
-    <button id = "detailBtn" class="detailButton">Detail</button>
-   </div>
-   </div>
-  
+    <button class="detailButton">      
+      Detail
+  </button>
 
-
+</div>
 
 </template>
 
